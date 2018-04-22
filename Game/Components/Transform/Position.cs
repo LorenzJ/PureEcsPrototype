672cfc534641a6e2214d15f6797f2e6aeefbb04a
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-22 22:55:28 +0200

    Add new components.

---
+++ Game/Components/Transform/Position.cs
@@ -0,0 +1,15 @@
+﻿using OpenGL;
+using TinyEcs;
+
+namespace Game.Components.Transform
+{
+    public struct Position : IComponent
+    {
+        public Vertex2f vector;
+
+        public Position(Vertex2f vector)
+        {
+            this.vector = vector;
+        }
+    }
+}
---
+++ Game/Components/Transform/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using OpenGL;
+using TinyEcs;
+
+namespace Game.Components.Transform
+{
+    public struct Rotation : IComponent
+    {
+        public Vertex2f vector;
+
+        public Rotation(Vertex2f vector)
+        {
+            this.vector = vector;
+        }
+    }
+}
