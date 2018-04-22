2cb391661a190225c6f52e34d66a0757c65477ad
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-22 22:33:55 +0200

    Fix attribute targets.

--- TinyEcs/Attributes.cs
+++ TinyEcs/Attributes.cs
@@ -5,14 +5,14 @@ namespace TinyEcs
     /// <summary>
     /// Mark a field as a target for simple dependency injection.
     /// </summary>
-    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class ResourceAttribute : Attribute
     {
     }
     /// <summary>
     /// Mark a field as an injected component group.
     /// </summary>
-    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class GroupAttribute : Attribute
     {
     }
