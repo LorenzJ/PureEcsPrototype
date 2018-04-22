ff6f7ffe3160682e92bd737e7bd448da6eb59ccf
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-22 19:48:00 +0200

    Rename RArray to RoArray

---
+++ TinyEcs/RoArray.cs
@@ -0,0 +1,25 @@
+﻿namespace TinyEcs
+{
+    /// <summary>
+    /// A read-only array of components.
+    /// </summary>
+    /// <typeparam name="T">Element type of the component array</typeparam>
+    public struct RoArray<T>
+    {
+        internal T[] items;
+
+        public RoArray(T[] items)
+        {
+            this.items = items;
+        }
+
+        public static implicit operator RoArray<T>(T[] items) => new RoArray<T>(items);
+
+        /// <summary>
+        /// Get a read-only reference to an item.
+        /// </summary>
+        /// <param name="i">Index of the item</param>
+        /// <returns></returns>
+        public ref readonly T this[int i] => ref items[i];
+    }
+}
