31b6ae9e15ed9053faf454a47e0f23b5c56fe76b
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-22 18:23:24 +0200

    Refactor world

---
+++ TinyEcs/World.GroupInjector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TinyEcs
+{
+    public partial class World
+    {
+        private class GroupInjector
+        {
+            private object groupObject;
+            private (Type, FieldInfo, ConstructorInfo)[] writeFields;
+            private (Type, FieldInfo, ConstructorInfo)[] readFields;
+            private FieldInfo entityField;
+            private FieldInfo lengthField;
+            internal ComponentGroup componentGroup;
+
+            private object[] arguments = new object[] { null };
+
+            public GroupInjector(object group, (Type, FieldInfo)[] writeFields, (Type, FieldInfo)[] readFields, FieldInfo entityField, FieldInfo lengthField, ComponentGroup componentGroup)
+            {
+                this.groupObject = group;
+                this.entityField = entityField;
+                this.lengthField = lengthField;
+                this.componentGroup = componentGroup;
+
+                var RwArrayType = typeof(RwArray<>);
+                this.writeFields = new(Type, FieldInfo, ConstructorInfo)[writeFields.Length];
+                for (var i = 0; i < writeFields.Length; i++)
+                {
+                    var (type, fieldInfo) = writeFields[i];
+                    var rawArrayType = type.MakeArrayType();
+                    var wrappedArrayType = RwArrayType.MakeGenericType(type);
+                    var constructor = wrappedArrayType.GetConstructor(new Type[] { rawArrayType });
+                    this.writeFields[i] = (type, fieldInfo, constructor);
+                }
+                var RArrayType = typeof(RArray<>);
+                this.readFields = new(Type, FieldInfo, ConstructorInfo)[readFields.Length];
+                for (var i = 0; i < readFields.Length; i++)
+                {
+                    var (type, fieldInfo) = readFields[i];
+                    var rawArrayType = type.MakeArrayType();
+                    var wrappedArrayType = RArrayType.MakeGenericType(type);
+                    var constructor = wrappedArrayType.GetConstructor(new Type[] { rawArrayType });
+                    this.readFields[i] = (type, fieldInfo, constructor);
+                }
+
+            }
+
+            public void Inject(Dictionary<Type, IComponentContainer> containers)
+            {
+                componentGroup.Inject(containers);
+                foreach (var (type, field, constructor) in writeFields)
+                {
+                    arguments[0] = componentGroup.GetDirectWrite(type);
+                    field.SetValue(groupObject, constructor.Invoke(arguments));
+                }
+                foreach (var (type, field, constructor) in readFields)
+                {
+                    arguments[0] = componentGroup.GetDirectRead(type);
+                    field.SetValue(groupObject, constructor.Invoke(arguments));
+                }
+                if (entityField != null)
+                {
+                    entityField.SetValue(groupObject, componentGroup.Entities);
+                }
+                if (lengthField != null)
+                {
+                    lengthField.SetValue(groupObject, componentGroup.Length);
+                }
+            }
+
+            public void Unpack(Dictionary<Type, IComponentContainer> containers)
+            {
+                componentGroup.Unpack(containers);
+            }
+
+        }
+
+
+    }
+
+
+}
--- TinyEcs/World.cs
+++ TinyEcs/World.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TinyEcs
 {
-    public class World
+    public partial class World
     {
         private const int initialSize = 2048;
         private int size = initialSize;
@@ -67,7 +67,6 @@ namespace TinyEcs
             Parallel.ForEach(injectors, injector => injector.Inject(componentContainerMap));
             Parallel.ForEach(systems, system => system.Execute(this, message));
             Parallel.ForEach(injectors, injector => injector.Unpack(componentContainerMap));
-
         }
 
         public static World Create()
@@ -86,25 +85,83 @@ namespace TinyEcs
                 .Where(type => type.IsValueType && type.GetInterfaces().Contains(typeof(IComponent)));
 
             var systems = new List<ISystem>();
-                
+
             var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            CreateComponentContainers();
+            CreateSystems();
 
-            foreach (var componentType in componentTypes)
+            world.systemMessageMap = systems
+                .ToLookup(system => system.GetType().BaseType.GetGenericArguments()[0]) as Lookup<Type, ISystem>;
+            return world;
+
+            void CreateComponentContainers()
             {
-                var componentContainerType = typeof(ComponentContainer<>).MakeGenericType(new Type[] { componentType });
-                var constructor = componentContainerType.GetConstructor(new Type[] { typeof(int) });
-                world.componentContainerMap.Add(componentType, constructor.Invoke(new object[] { initialSize }) as IComponentContainer);
+                foreach (var componentType in componentTypes)
+                {
+                    var componentContainerType = typeof(ComponentContainer<>).MakeGenericType(new Type[] { componentType });
+                    var constructor = componentContainerType.GetConstructor(new Type[] { typeof(int) });
+                    world.componentContainerMap.Add(componentType, constructor.Invoke(new object[] { initialSize }) as IComponentContainer);
+                }
             }
-            foreach (var systemType in systemTypes)
+
+            void CreateSystems()
             {
-                var systemInstance = systemType.GetConstructor(Type.EmptyTypes).Invoke(null);
-                systems.Add(systemInstance as ISystem);
+                foreach (var systemType in systemTypes)
+                {
+                    var systemInstance = systemType.GetConstructor(Type.EmptyTypes).Invoke(null);
+                    systems.Add(systemInstance as ISystem);
 
-                // Find all fields for resource injection
-                var resourceFields =
-                    systemType.GetFields(bindingFlags)
-                    .Where(fi => fi.GetCustomAttribute(typeof(ResourceAttribute)) != null);
+                    // Find all fields for resource injection
+                    var resourceFields =
+                        systemType.GetFields(bindingFlags)
+                        .Where(fi => fi.GetCustomAttribute(typeof(ResourceAttribute)) != null);
+                    InjectResources(systemInstance, resourceFields);
 
+                    // Find groups for component injection
+                    var groupFields =
+                        systemType.GetFields(bindingFlags)
+                        .Where(fi => fi.GetCustomAttribute(typeof(GroupAttribute)) != null);
+
+                    var groupInjectors = new List<GroupInjector>();
+                    CreateComponentGroups();
+                    world.groupInjectorMap.Add(systemInstance as ISystem, groupInjectors);
+
+                    void CreateComponentGroups()
+                    {
+                        foreach (var groupField in groupFields)
+                        {
+                            var groupFieldType = groupField.FieldType;
+                            var groupFieldInstance = groupFieldType.GetConstructor(Type.EmptyTypes).Invoke(null);
+                            groupField.SetValue(systemInstance, groupFieldInstance);
+
+                            var fields = groupFieldType.GetFields(bindingFlags);
+                            var lengthField = groupFieldType.GetField("length");
+                            var entityField = fields.Where(fi => fi.FieldType == typeof(Entity[])).SingleOrDefault();
+                            var genericFields = fields
+                                .Where(fi => fi.FieldType.IsGenericType);
+                            var readFields = genericFields
+                                .Where(fi => fi.FieldType.GetGenericTypeDefinition() == typeof(RArray<>));
+                            var writeFields = genericFields
+                                .Where(fi => fi.FieldType.GetGenericTypeDefinition() == typeof(RwArray<>));
+
+                            var readTypes = readFields
+                                .Select(fi => fi.FieldType.GetGenericArguments()[0]).ToArray();
+                            var writeTypes = writeFields
+                                .Select(fi => fi.FieldType.GetGenericArguments()[0]).ToArray();
+
+                            var readTuples = readTypes.Zip(readFields, (t, f) => (t, f)).ToArray();
+                            var writeTuples = writeTypes.Zip(writeFields, (t, f) => (t, f)).ToArray();
+
+                            var componentGroup = world.GetComponentGroup(readTypes, writeTypes);
+                            var groupInjector = new GroupInjector(groupFieldInstance, writeTuples, readTuples, entityField, lengthField, componentGroup);
+                            groupInjectors.Add(groupInjector);
+                        }
+                    }
+                }  
+            }
+
+            void InjectResources(object systemInstance, IEnumerable<FieldInfo> resourceFields)
+            {
                 foreach (var resourceField in resourceFields)
                 {
                     // Create or get the resource
@@ -116,47 +173,7 @@ namespace TinyEcs
                     // Inject the resource
                     resourceField.SetValue(systemInstance, resource);
                 }
-
-                // Find groups for component injection
-                var groupFields =
-                    systemType.GetFields(bindingFlags)
-                    .Where(fi => fi.GetCustomAttribute(typeof(GroupAttribute)) != null);
-
-                var groupInjectors = new List<GroupInjector>();
-                foreach (var groupField in groupFields)
-                {
-                    var groupFieldType = groupField.FieldType;
-                    var groupFieldInstance = groupFieldType.GetConstructor(Type.EmptyTypes).Invoke(null);
-                    groupField.SetValue(systemInstance, groupFieldInstance);
-
-                    var fields = groupFieldType.GetFields(bindingFlags);
-                    var lengthField = groupFieldType.GetField("length");
-                    var entityField = fields.Where(fi => fi.FieldType == typeof(Entity[])).SingleOrDefault();
-                    var genericFields = fields
-                        .Where(fi => fi.FieldType.IsGenericType);
-                    var readFields = genericFields
-                        .Where(fi => fi.FieldType.GetGenericTypeDefinition() == typeof(RArray<>));
-                    var writeFields = genericFields
-                        .Where(fi => fi.FieldType.GetGenericTypeDefinition() == typeof(RwArray<>));
-
-                    var readTypes = readFields
-                        .Select(fi => fi.FieldType.GetGenericArguments()[0]).ToArray();
-                    var writeTypes = writeFields
-                        .Select(fi => fi.FieldType.GetGenericArguments()[0]).ToArray();
-
-                    var readTuples = readTypes.Zip(readFields, (t, f) => (t, f)).ToArray();
-                    var writeTuples = writeTypes.Zip(writeFields, (t, f) => (t, f)).ToArray();
-
-                    var componentGroup = world.GetComponentGroup(readTypes, writeTypes);
-                    var groupInjector = new GroupInjector(groupFieldInstance, writeTuples, readTuples, entityField, lengthField, componentGroup);
-                    groupInjectors.Add(groupInjector);
-                }
-                world.groupInjectorMap.Add(systemInstance as ISystem, groupInjectors);
             }
-
-            world.systemMessageMap = systems
-                .ToLookup(system => system.GetType().BaseType.GetGenericArguments()[0]) as Lookup<Type, ISystem>;
-            return world;
         }
 
         private ComponentGroup GetComponentGroup(Type[] readTypes, Type[] writeTypes)
@@ -172,77 +189,6 @@ namespace TinyEcs
                 componentGroups.Add(newComponentGroup);
                 return newComponentGroup;
             }
-        }
-
-        private class GroupInjector
-        {
-            private object group;
-            private (Type, FieldInfo, ConstructorInfo)[] writeFields;
-            private (Type, FieldInfo, ConstructorInfo)[] readFields;
-            private FieldInfo entityField;
-            private FieldInfo lengthField;
-            internal ComponentGroup componentGroup;
-
-            private object[] arguments = new object[] { null };
-
-            public GroupInjector(object group, (Type, FieldInfo)[] writeFields, (Type, FieldInfo)[] readFields, FieldInfo entityField, FieldInfo lengthField, ComponentGroup componentGroup)
-            {
-                this.group = group;
-                this.entityField = entityField;
-                this.lengthField = lengthField;
-                this.componentGroup = componentGroup;
-
-                var RwArrayType = typeof(RwArray<>);
-                this.writeFields = new(Type, FieldInfo, ConstructorInfo)[writeFields.Length];
-                for (var i = 0; i < writeFields.Length; i++)
-                {
-                    var (type, fieldInfo) = writeFields[i];
-                    var rawArrayType = type.MakeArrayType();
-                    var wrappedArrayType = RwArrayType.MakeGenericType(type);
-                    var constructor = wrappedArrayType.GetConstructor(new Type[] { rawArrayType });
-                    this.writeFields[i] = (type, fieldInfo, constructor);
-                }
-                var RArrayType = typeof(RArray<>);
-                this.readFields = new(Type, FieldInfo, ConstructorInfo)[readFields.Length];
-                for (var i = 0; i < readFields.Length; i++)
-                {
-                    var (type, fieldInfo) = readFields[i];
-                    var rawArrayType = type.MakeArrayType();
-                    var wrappedArrayType = RArrayType.MakeGenericType(type);
-                    var constructor = wrappedArrayType.GetConstructor(new Type[] { rawArrayType });
-                    this.readFields[i] = (type, fieldInfo, constructor);
-                }
-
-            }
-
-            public void Inject(Dictionary<Type, IComponentContainer> containers)
-            {
-                componentGroup.Inject(containers);
-                foreach (var (type, field, constructor) in writeFields)
-                {
-                    arguments[0] = componentGroup.GetDirectWrite(type);
-                    field.SetValue(group, constructor.Invoke(arguments));
-                }
-                foreach (var (type, field, constructor) in readFields)
-                {
-                    arguments[0] = componentGroup.GetDirectRead(type);
-                    field.SetValue(group, constructor.Invoke(arguments));
-                }
-                if (entityField != null)
-                {
-                    entityField.SetValue(group, componentGroup.Entities);
-                }
-                if (lengthField != null)
-                {
-                    lengthField.SetValue(group, componentGroup.Length);
-                }
-            }
-
-            public void Unpack(Dictionary<Type, IComponentContainer> containers)
-            {
-                componentGroup.Unpack(containers);
-            }
-
         }
 
 
