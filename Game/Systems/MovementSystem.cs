a3546f2ce1161167ad4f79855a5b8dd74134bb68
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-22 22:53:57 +0200

    Make MovementSystem more succinct.

--- Game/Systems/MovementSystem.cs
+++ Game/Systems/MovementSystem.cs
@@ -4,25 +4,22 @@ using Game.Components.Transform;
 
 namespace Game.Systems
 {
-    public class MovementSystem : System<UpdateMessage>
+    public class MovementSystem : ComponentSystem<UpdateMessage>
     {
 
-        class Data
+        public class Data
         {
-            public int length = default;
-            public RwArray<Position> positions = default;
-            public RoArray<Heading> directions = default;
+            public int length;
+            public RwArray<Position> positions;
+            public RoArray<Heading> directions;
         }
-        [Group] Data data;
+        [Group] public Data data;
 
         protected override void Execute(World world, UpdateMessage message)
         {
-            var positions = data.positions;
-            var directions = data.directions;
-
             Parallel.For(0, data.length, i =>
             {
-                positions[i].vector += directions[i].vector * message.DeltaTime;
+                data.positions[i].vector += data.directions[i].vector * message.DeltaTime;
             });
         }
     }
