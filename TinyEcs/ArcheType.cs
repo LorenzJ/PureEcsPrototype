4e4fbf9d80d9eb2b3028acf239e5d136b102b32e
LorenzJ <lorenz_jolling@hotmail.com>
2018-04-23 10:04:08 +0200

    Fix archetype

--- TinyEcs/ArcheType.cs
+++ TinyEcs/ArcheType.cs
@@ -2,17 +2,19 @@
 
 namespace TinyEcs
 {
-    public class ArcheType : IEquatable<ArcheType>
+    public class Archetype : IEquatable<Archetype>
     {
         private int id;
-        
-        internal ArcheType(int id)
+        internal Type[] types;
+
+        internal Archetype(int id, Type[] types)
         {
             this.id = id;
+            this.types = types;
         }
 
-        public override bool Equals(object obj) => Equals(obj as ArcheType);
-        public bool Equals(ArcheType other) => other != null && id == other.id;
+        public override bool Equals(object obj) => Equals(obj as Archetype);
+        public bool Equals(Archetype other) => other != null && id == other.id;
         public override int GetHashCode() => 1877310944 + id.GetHashCode();
     }
 }
